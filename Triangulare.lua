aeff501efa12915610284064158a40cf55b12121
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-05-27 14:54:57 -0300

    update Triangulare.lua

--- Triangulare.lua
+++ Triangulare.lua
@@ -1,27 +1,3 @@
-local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
-local Window = WindUI:CreateWindow({
-  Title = "Triangulare | Universal",
-  Icon = "triangle",
-  Author = "by Moligrafi",
-  Folder = "Triangulare",
-  Size = UDim2.fromOffset(580, 460),
-  Transparent = true,
-  Theme = "Dark",
-  User = {
-    Enabled = true
-  },
-  SideBarWidth = 200,
-  HasOutline = true,
-})
-Window:EditOpenButton({
-  Title = "Triangulare",
-  Icon = "triangle",
-  CornerRadius = UDim.new(0,16),
-  StrokeThickness = 2,
-  Color = ColorSequence.new(Color3.fromRGB(0, 255, 120), Color3.fromRGB(0, 120, 255)),
-  Draggable = true
-})
-
 -- Locals
 local eu = game:GetService("Players").LocalPlayer
 

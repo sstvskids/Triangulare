0b82bdaadeb51640862baa77c483a238d556e07c
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-05-27 13:52:30 -0300

    update Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,13 @@
+local function LoadScript(path)
+  local Script = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Triangulare/main/" .. game:GetService("HttpService"):UrlEncode(path))
+  local Credits = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Triangulare/main/extra/Credits.lua")
+  loadstring(Script .. Credits)()
+end
+
+local Game = game.GameId
+
+if Game == 7516718402 then
+  LoadScript("games/Noobs Must Die.lua")
+else
+  LoadScript("Triangulare.lua")
+end

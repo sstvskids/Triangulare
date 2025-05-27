44d76a3f856e2876d4014a16a1b75aad07e031ce
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-05-27 15:06:05 -0300

    update extra/Credits.lua

--- extra/Credits.lua
+++ extra/Credits.lua
@@ -1,27 +1,26 @@
-Tabs.ExtraDivider = Window:Divider()
-
--- Report
-Tabs.ReportTab = Window:Tab({ Title = "Report", Icon = "bug"})
-
--- Credits
-Tabs.CreditsTab = Window:Tab({ Title = "Credits", Icon = "info"})
-Tabs.CreditsTab:Section({ Title = "Developers" })
+if Tabs.Settings.Report then
+  Tabs.ReportTab = Window:Tab({ Title = "Report", Icon = "bug"})
+end
+if Tabs.Settings.Credits then
+  Tabs.CreditsTab = Window:Tab({ Title = "Credits", Icon = "info"})
+  Tabs.CreditsTab:Section({ Title = "Developers" })
   Tabs.CreditsTab:Paragraph({
-  Title = "Founder Developer",
-  Desc = "Discord: @moligrafi",
-})
-Tabs.CreditsTab:Section({ Title = "Discord Server" })
-Tabs.CreditsTab:Paragraph({
-  Title = "Discord Server",
-  Desc = "https://discord.gg/linkdoserver",
-  Buttons = {
-    {
-      Title = "Copy Server Link",
-      Variant = "Primary",
-      Callback = function()
-        setclipboard("https://discord.gg/linkdoserver")
-      end,
-      Icon = "link"
-    }
-  }
-})
+      Title = "Founder Developer",
+      Desc = "Discord: @moligrafi",
+  })
+  Tabs.CreditsTab:Section({ Title = "Discord Server" })
+  Tabs.CreditsTab:Paragraph({
+      Title = "Discord Server",
+      Desc = "https://discord.gg/P9fdBwqcCs",
+      Buttons = {
+        {
+          Title = "Copy Server Link",
+          Variant = "Primary",
+          Callback = function()
+            setclipboard("https://discord.gg/P9fdBwqcCs")
+          end,
+          Icon = "link"
+      }
+    } 
+  })
+end

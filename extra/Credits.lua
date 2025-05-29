c580d7fff620f4ef3c343364d01801d1969b0bcf
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-05-29 16:15:44 -0300

    update extra/Credits.lua

--- extra/Credits.lua
+++ extra/Credits.lua
@@ -7,33 +7,9 @@ local Settings = {
 -- Supported
 local function SupportedList(type)
   local gamePlaceIds = {
-    ["Ninja Legends"] = 3956818381,
-    ["The Upgrade Tree Of Tree"] = 16148053600,
-    ["Everything Upgrade Tree"] = 122809141833750,
-    ["Snow Plow Simulator"] = 11701792069,
-    ["Farm for Fun! 🌾"] = 6598746935,
-    ["⚔️ Slash Mobs Simulator"] = 81257648942232,
-    ["Rune Inc"] = 101162558216961,
-    ["Make and Sell Cars"] = 109819539837829,
-    ["Find The Buttons! 🔎🔴"] = 91314495602934,
-    ["🥊Punch Monsters!"] = 8034886758,
-    ["Cash Incremental"] = 129159449618378,
-    ["Legends Of Speed"] = 3101667897,
-    ["Find The Buttons!"] = 112730892056697,
-    ["Clicking Quest!"] = 79274333046533,
-    ["Growth Incremental"] = 112808176368279,
-    ["Find The Button"] = 87643681021528,
-    ["Snow Incremental Simulator"] = 138763709974342,
-    ["Ultimate Upgrade Tree"] = 129503100059800,
-    ["Jump Incremental"] = 98896743739347,
-    ["Find Chicken Nuggets"] = 107400840408672,
-    ["Find Buttons! 👀"] = 104584676217962,
-    ["Vyasa"] = 12398408187,
-    ["Computer Upgrade Tree"] = 18242944461,
-    ["Swords Battle Simulator"] = 105628647191901,
-    ["Dungeons of Doom"] = 77293138169730,
-    ["Flee The Facility"] = 893973440,
-    ["City Destroyer Simulator"] = 15148585624
+    ["Noobs Must Die"] = 123,
+    ["Rope Battles"] = 123,
+    ["Dangerous Night"] = 123
   }
   if type == "Names" then
     local Names = {}

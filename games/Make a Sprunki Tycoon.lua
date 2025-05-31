874126abec3d256efb060e09669db0fbfa2748e8
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-05-31 17:02:09 -0300

    update games/Make a Sprunki Tycoon.lua

---
+++ games/Make a Sprunki Tycoon.lua
@@ -0,0 +1,8 @@
+-- 71508074112900 place
+-- 7606156849 game
+workspace.Map.Tycoons["HallowHub's Tycoon"]["Worker 1 Button"]
+game:GetService("Players").LocalPlayer.leaderstats.Money.Value
+workspace.Map.Tycoons["HallowHub's Tycoon"]["Pile of Rubble 1"].PromptPart.ProximityPrompt
+workspace.Map.Tycoons["HallowHub's Tycoon"].Conveyor1["Insert Rocks"].BillboardGui.Amount Add Fabric (0/3)
+workspace.Map.Tycoons["HallowHub's Tycoon"]["Worker 1"].HumanoidRootPart.WakeupPrompt
+workspace.Npcs["2025110"].HumanoidRootPart.ProximityPrompt

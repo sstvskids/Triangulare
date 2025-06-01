843b3fa9a50675658a484e089a58de9c215c1d2f
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-05-31 21:13:30 -0300

    update games/Make a Sprunki Tycoon.lua

--- games/Make a Sprunki Tycoon.lua
+++ games/Make a Sprunki Tycoon.lua
@@ -6,43 +6,68 @@ getgenv().BetterWorkers = false
 
 -- Locals
 local eu = game:GetService("Players").LocalPlayer
+local Settings = {
+  Conveyor1 = true,
+  Conveyor2 = true
+}
 
 -- Functions
+local function GetConveryor(instance)
+  if string.find(instance.Name, "Worker") then
+    return
+  elseif string.find(instance.Name, "Button") then
+    return
+  end
+end
 local function AutoSell()
   while getgenv().AutoSell and task.wait(1) do
-    if eu.Character:FindFirstChild("Sprunki") or eu.Backpack:FindFirstChild("Sprunki") then
-      for _, npc in pairs(workspace.Npcs:GetChildren()) do
-        if npc.HumanoidRootPart.ProximityPrompt.Enabled then
-          fireproximityprompt(npc.HumanoidRootPart.ProximityPrompt)
-          if not eu.Character:FindFirstChild("Sprunki") and not eu.Backpack:FindFirstChild("Sprunki") then break end
+    pcall(function()
+      if eu.Character:FindFirstChild("Sprunki") or eu.Backpack:FindFirstChild("Sprunki") then
+        for _, npc in pairs(workspace.Npcs:GetChildren()) do
+          if npc.HumanoidRootPart.ProximityPrompt.Enabled then
+            fireproximityprompt(npc.HumanoidRootPart.ProximityPrompt)
+            if not eu.Character:FindFirstChild("Sprunki") and not eu.Backpack:FindFirstChild("Sprunki") then break end
+          end
         end
       end
-    end
+    end)
   end
 end
 local function GetOre()
   while getgenv().GetOre and task.wait(1) do
-    if not eu.Character:FindFirstChild("Fabric") and not eu.Backpack:FindFirstChild("Fabric") then
-      fireproximityprompt(workspace.Map.Tycoons[eu.Name .. "'s Tycoon"]["Pile of Rubble 1"].PromptPart.ProximityPrompt)
-    end
+    pcall(function()
+      if not eu.Character:FindFirstChild("Fabric") and not eu.Backpack:FindFirstChild("Fabric") then
+        fireproximityprompt(workspace.Map.Tycoons[eu.Name .. "'s Tycoon"]["Pile of Rubble 1"].PromptPart.ProximityPrompt)
+      end
+    end)
   end
 end
 local function BetterWorkers()
   while getgenv().BetterWorkers and task.wait(1) do
-    for _, worker in pairs(workspace.Map.Tycoons[eu.Name .. "'s Tycoon"]:GetChildren()) do
-      if string.find(worker.Name, "Worker") and worker.HumanoidRootPart.WakeupPrompt.Enabled == true then
-        fireproximityprompt(worker.HumanoidRootPart.WakeupPrompt)
+    pcall(function()
+      for _, worker in pairs(workspace.Map.Tycoons[eu.Name .. "'s Tycoon"]:GetChildren()) do
+        if string.find(worker.Name, "Worker") and worker.HumanoidRootPart.WakeupPrompt.Enabled == true then
+          fireproximityprompt(worker.HumanoidRootPart.WakeupPrompt)
+        end
       end
-    end
+    end)
   end
 end
 local function AutoBuy()
   while getgenv().AutoBuy and task.wait(1) do
-    for _, button in pairs(workspace.Map.Tycoons[eu.Name .. "'s Tycoon"]:GetChildren()) do
-      if string.find(button.Name, "Button") and button:GetAttribute("Price") <= eu.leaderstats.Money.Value then
-        firetouchinterest(eu.Character.HumanoidRootPart, button.Head, 0)
-        firetouchinterest(eu.Character.HumanoidRootPart, button.Head, 1)
+    pcall(function()
+      for _, button in pairs(workspace.Map.Tycoons[eu.Name .. "'s Tycoon"]:GetChildren()) do
+        if string.find(button.Name, "Button") and button:GetAttribute("Price") <= eu.leaderstats.Money.Value then
+          firetouchinterest(eu.Character.HumanoidRootPart, button.Head, 0)
+          firetouchinterest(eu.Character.HumanoidRootPart, button.Head, 1)
+        end
       end
+    end)
+  end
+end
+local function CollectSprunkis()
+  while getgenv().CollectSprunkis and task.wait(1) do
+    if not eu.Character:FindFirstChild("Sprunki") and not eu.Backpack:FindFirstChild("Sprunki") then
     end
   end
 end
@@ -58,7 +83,8 @@ workspace.Npcs["2025110"].HumanoidRootPart.ProximityPrompt
 
 -- Tabs
 local Tabs = {
-  Menu = Window:Tab({ Title = "Main", Icon = "home"})
+  Menu = Window:Tab({ Title = "Main", Icon = "house"}),
+  Sprunkis = Window:Tab({ Title = "Sprunkis", Icon = "panda"})
 }
 Window:SelectTab(1)
 
@@ -83,6 +109,18 @@ Tabs.Menu:Toggle({
   end
 })
 Tabs.Menu:Toggle({
+  Title = "Awake Workers",
+  Desc = "Automatically wake up workers.",
+  Value = false,
+  Callback = function(state)
+    getgenv().BetterWorkers = state
+    BetterWorkers()
+  end
+})
+
+-- Menu
+Tabs.Sprunkis:Section({ Title = "Auto Farm" })
+Tabs.Sprunkis:Toggle({
   Title = "Auto Sell",
   Desc = "Automatically sells Sprunkis.",
   Value = false,
@@ -91,12 +129,12 @@ Tabs.Menu:Toggle({
     AutoSell()
   end
 })
-Tabs.Menu:Toggle({
-  Title = "Awake Workers",
-  Desc = "Automatically wake up workers.",
+Tabs.Sprunkis:Toggle({
+  Title = "Auto Collect",
+  Desc = "Automatically collect Sprunkis.",
   Value = false,
   Callback = function(state)
-    getgenv().BetterWorkers = state
-    BetterWorkers()
+    getgenv().CollectSprunkis = state
+    CollectSprunkis()
   end
 })

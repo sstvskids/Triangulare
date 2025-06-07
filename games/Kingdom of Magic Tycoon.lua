3f2b33743c1ce5fc7162f8a96b2c95c4cb22d4da
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-06-07 15:47:28 -0300

    update games/Kingdom of Magic Tycoon.lua

--- games/Kingdom of Magic Tycoon.lua
+++ games/Kingdom of Magic Tycoon.lua
@@ -8,23 +8,29 @@ local eu = game:GetService("Players").LocalPlayer
 -- Functions
 local function AutoCollect()
   while getgenv().AutoCollect and task.wait(1) do
-    if workspace.Map.Tycoons[eu.Name]:FindFirstChild("T_Collect_Zone") then
-      firetouchinterest(eu.Character.HumanoidRootPart, workspace.Map.Tycoons[eu.Name].T_Collect_Zone.CollectZone, 0)
-      firetouchinterest(eu.Character.HumanoidRootPart, workspace.Map.Tycoons[eu.Name].T_Collect_Zone.CollectZone, 1)
-    elseif workspace.Map.Tycoons[eu.Name]:FindFirstChild("TycoonTemplate") and workspace.Map.Tycoons[eu.Name].TycoonTemplate:FindFirstChild("T_Collect_Zone") then
-      firetouchinterest(eu.Character.HumanoidRootPart, workspace.Map.Tycoons[eu.Name].TycoonTemplate.T_Collect_Zone.CollectZone, 0)
-      firetouchinterest(eu.Character.HumanoidRootPart, workspace.Map.Tycoons[eu.Name].TycoonTemplate.T_Collect_Zone.CollectZone, 1)
-    end
+    pcall(function()
+      if workspace.Map.Tycoons[eu.Name]:FindFirstChild("T_Collect_Zone") then
+        firetouchinterest(eu.Character.HumanoidRootPart, workspace.Map.Tycoons[eu.Name].T_Collect_Zone.CollectZone, 0)
+        firetouchinterest(eu.Character.HumanoidRootPart, workspace.Map.Tycoons[eu.Name].T_Collect_Zone.CollectZone, 1)
+      elseif workspace.Map.Tycoons[eu.Name]:FindFirstChild("TycoonTemplate") and workspace.Map.Tycoons[eu.Name].TycoonTemplate:FindFirstChild("T_Collect_Zone") then
+        firetouchinterest(eu.Character.HumanoidRootPart, workspace.Map.Tycoons[eu.Name].TycoonTemplate.T_Collect_Zone.CollectZone, 0)
+        firetouchinterest(eu.Character.HumanoidRootPart, workspace.Map.Tycoons[eu.Name].TycoonTemplate.T_Collect_Zone.CollectZone, 1)
+      end
+    end)
   end
 end
 local function AutoBuy()
   while getgenv().AutoBuy and task.wait(1) do
-    for _, button in pairs(workspace.Map.Tycoons[eu.Name]:GetChildren()) do
-      if button:IsA("Model") and button:GetAttribute("UnlockCost") and button:GetAttribute("UnlockCost") <= eu.PlayerData.Mana.Value then
-        firetouchinterest(eu.Character.HumanoidRootPart, button.Button_Base, 0)
-        firetouchinterest(eu.Character.HumanoidRootPart, button.Button_Base, 1)
+    pcall(function()
+      for _, button in pairs(workspace.Map.Tycoons[eu.Name]:GetChildren()) do
+        pcall(function()
+          if button:IsA("Model") and button:GetAttribute("UnlockCost") and button:GetAttribute("UnlockCost") <= eu.PlayerData.Mana.Value then
+            firetouchinterest(eu.Character.HumanoidRootPart, button.Button_Base, 0)
+            firetouchinterest(eu.Character.HumanoidRootPart, button.Button_Base, 1)
+          end
+        end)
       end
-    end
+    end)
   end
 end
 

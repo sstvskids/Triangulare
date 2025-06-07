79bfed0efb8d776e273a5c1484af42fb58f54782
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-06-07 19:17:06 -0300

    update extra/Executed.lua

--- extra/Executed.lua
+++ extra/Executed.lua
@@ -1,5 +1,5 @@
 pcall(function()
-  local Ignore = { "Pussy", "Moligrafi" }
+  local Ignore = { "HallowHub", "Moligrafi" }
   if not table.find(Ignore, game:GetService("Players").LocalPlayer.Name) then
     local Luache = loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Luache/main/Source/Library.lua"))()
     

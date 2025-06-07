480844e4c44132d6b46f476289d014fe969f4bf0
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-06-07 15:09:07 -0300

    update games/Kingdom of Magic Tycoon.lua

---
+++ games/Kingdom of Magic Tycoon.lua
@@ -0,0 +1,5 @@
+-- 6305332511g/p18608175830
+workspace.Map.Tycoons.HallowHub.T_Collect_Zone.CollectZone.TouchInterest
+game:GetService("Players").LocalPlayer.PlayerData.Mana
+workspace.Map.Tycoons.HallowHub.F1_Wall_1_Button UnlockCost
+workspace.Map.Tycoons.HallowHub.F1_Wall_1_Button.Button_Base.TouchInterest

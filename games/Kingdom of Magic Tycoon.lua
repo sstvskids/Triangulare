5f73aef1f8e4625a886de7549646742dd0886b99
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-06-07 15:36:05 -0300

    update games/Kingdom of Magic Tycoon.lua

--- games/Kingdom of Magic Tycoon.lua
+++ games/Kingdom of Magic Tycoon.lua
@@ -8,22 +8,18 @@ local eu = game:GetService("Players").LocalPlayer
 -- Functions
 local function AutoCollect()
   while getgenv().AutoCollect and task.wait(1) do
-    pcall(function()
-      firetouchinterest(eu.Character.HumanoidRootPart, workspace.Map.Tycoons[eu.Name].T_Collect_Zone.CollectZone, 0)
-      firetouchinterest(eu.Character.HumanoidRootPart, workspace.Map.Tycoons[eu.Name].T_Collect_Zone.CollectZone, 1)
-    end)
+    firetouchinterest(eu.Character.HumanoidRootPart, workspace.Map.Tycoons[eu.Name].T_Collect_Zone.CollectZone, 0)
+    firetouchinterest(eu.Character.HumanoidRootPart, workspace.Map.Tycoons[eu.Name].T_Collect_Zone.CollectZone, 1)
   end
 end
 local function AutoBuy()
   while getgenv().AutoBuy and task.wait(1) do
-    pcall(function()
-      for _, button in pairs(workspace.Map.Tycoons[eu.Name]:GetChildren()) do
-        if button:IsA("Model") and button:GetAttribute("UnlockCost") <= eu.PlayerData.Mana.Value then
-          firetouchinterest(eu.Character.HumanoidRootPart, butob.Button_Base, 0)
-          firetouchinterest(eu.Character.HumanoidRootPart, butob.Button_Base, 1)
-        end
+    for _, button in pairs(workspace.Map.Tycoons[eu.Name]:GetChildren()) do
+      if button:IsA("Model") and button:GetAttribute("UnlockCost") <= eu.PlayerData.Mana.Value then
+        firetouchinterest(eu.Character.HumanoidRootPart, butob.Button_Base, 0)
+        firetouchinterest(eu.Character.HumanoidRootPart, butob.Button_Base, 1)
       end
-    end)
+    end
   end
 end
 
@@ -41,7 +37,7 @@ local Tabs = {
 Window:SelectTab(1)
 
 -- Menu
-Tabs.Menu:Section({ Title = "Helpful" })
+Tabs.Menu:Section({ Title = "Helpfual" })
 Tabs.Menu:Toggle({
   Title = "Auto Collect Mana",
   Desc = "Automatically collect mana.",

42dad089379536191b59cc627e1c58adc16f34b8
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-06-08 18:06:49 -0300

    update games/Wizard Tycoon 2 Player.lua

---
+++ games/Wizard Tycoon 2 Player.lua
@@ -0,0 +1,18 @@
+local args = {
+    [1] = CFrame.new(-131.27867126464844, 70.39132690429688, 160.02992248535156) * CFrame.Angles(-3.130964517593384, 0.7702462077140808, 3.1341919898986816),
+    [2] = 100,
+    [3] = 1.5,
+    [4] = game:GetService("Players").LocalPlayer.Character.Wand,
+    [5] = 15,
+    [6] = game:GetService("Players").LocalPlayer.Character
+}
+
+game:GetService("Players").LocalPlayer.Character.Wand.Fire:FireServer(unpack(args))
+game:GetService("Players").LocalPlayer.leaderstats.Cash.Value
+workspace["berezaa's Tycoon Kit"]["Pastel Blue"].BlueSecond.Owner
+workspace["berezaa's Tycoon Kit"]["Pastel Blue"].BlueFirst.Owner
+workspace["berezaa's Tycoon Kit"]["Pastel Blue"].Essentials.Giver2
+workspace["berezaa's Tycoon Kit"]["Pastel Blue"].BlueSecond.Buttons["Broom - $5000"].Price.Value
+
+workspace["berezaa's Tycoon Kit"]["Pastel Blue"].BlueSecond.Buttons["Broom - $5000"].Head
+110988953/281489669

0be43d7d0ef522f30c6cdd0829bdeb147e32738e
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-06-11 22:57:32 -0300

    update games/raise bob.lua

---
+++ games/raise bob.lua
@@ -0,0 +1,8 @@
+workspace.Utilities.Cash.Cash.TouchInterest
+workspace.Utilities.Poop["5375b4bf-ad20-472c-80d2-7013c76bca87"].ProximityPrompt
+local args = {
+    [1] = "57cf708d-6d47-4b6e-bfad-cd31ee60f057"
+}
+
+game:GetService("ReplicatedStorage").Remotes.Bomb_Click:FireServer(unpack(args))
+workspace.Utilities.Bombs["57cf708d-6d47-4b6e-bfad-cd31ee60f057"].Model.Head.Pet

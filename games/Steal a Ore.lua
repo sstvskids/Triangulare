52ff3ca82aaada0b03dd2c9040800bb31cc45df5
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-06-14 13:18:52 -0300

    update games/Steal a Ore.lua

---
+++ games/Steal a Ore.lua
@@ -0,0 +1,6 @@
+7790982864/123763291847901
+workspace.Bases.Base2.Slots.Collector1.TouchInterest
+workspace.Bases.Base2.Spawn.BaseUI.Enabled
+Your Base
+workspace.Bases.Base2.LockPart.BaseLockUI.Value.Visible == false
+workspace.Bases.Base2.Gate:GetChildren()[18].Transparency == 1, 0.3
